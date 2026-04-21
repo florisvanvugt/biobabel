ab51d322b493651f8d0ede05d988f9bf7aa3e44b
Floris <ftvanvugt@gmail.com>
2026-04-21 15:22:28 -0400

    Revamping the XDF LSL loader script so that it doesn't truncate data.

--- src/biobabel/load_lsl.py
+++ src/biobabel/load_lsl.py
@@ -5,100 +5,124 @@ import datetime
 
 
 import pyxdf  # pip3 install pyxdf
+from scipy.interpolate import interp1d
+
+
 
 
 def load(fname):
     """Load LSL format XDF file."""
 
-    print("ATTENTION, this is a somewhat case-specific script.")
-    print("It might not work in general to read XDF files.")
-    print("Make sure you know what you are doing if you proceed here.")
-
     bio = biobabel.Biodata()  # create a new biodata object
     bio.name = fname
 
     # This file is included in bioread
-    streams, header = pyxdf.load_xdf(fname)
-    print(header)
+    streams, header = pyxdf.load_xdf(fname,
+                                     dejitter_timestamps=True, # should be default, but just to be sure
+                                     synchronize_clocks=True
+                                     )
+    #print(header)
 
-    participants = [s["info"]["name"][0] for s in streams]
+
+    
+    participants = [ stringify(s["info"]["name"]) for s in streams]
     participants.sort()
     bio.participants = participants
 
     # Determine the common time onset of signals, so that we can then crop to that.
-    ONSET_T, N_SAMP = get_onset(streams)
+    ONSET_T, OFFSET_T = get_onset_offset(streams)
     # print(ONSET_T,N_SAMP) # debug
 
     for s in streams:
-        info = s["info"]
-        print(" {} {}".format(info["name"][0], info["type"][0]))
-        tp = info["type"][0].lower()
-        # if tp!='ecg': continue # for now we only accept ECG signals
+        info = s["info"] # Fetch metadata about this stream
+        stream_name = stringify(info["name"])
+        stream_type = stringify(info["type"]).lower()
+        dtype = stringify(info['channel_format'])
+        if dtype=='string':
+            continue # work with strings later # TODO
 
-        modality = tp
-        p = info["name"][0]
+        print(" {} {}".format(stream_name,stream_type))
+        modality = biobabel.guess_modality(stream_type)
+        
+        p = stream_name
+        
+        # Find out which time stamps are within the time range
+        rawdata = s["time_series"].T[0]#[
+        #t_sel
+        #]  # just take the first stream # TODO: This could be expanded to extract multiple streams
+        timestamps = s['time_stamps']
 
-        t_sel = s["time_stamps"] >= ONSET_T
-        rawdata = s["time_series"].T[0][
-            t_sel
-        ]  # just take the first stream, and only after the common starting point
-        # print(rawdata.shape) # debug
-        rawdata = rawdata[:N_SAMP]  # take only the common chunk
-        sz = rawdata.shape[0]
-        # print(sz) # debug
-        assert sz == N_SAMP
-
-        SR = float(info["nominal_srate"][0])  # info['effective_srate']
-        units = info["desc"][0]["channels"][0]["channel"][0]["unit"][0]
+        SR = info['effective_srate'] # take the effective sampling rate since the nominal can be off or even zero
+        
+        # Use interpolation to get a regular sampled grid determined by the effective sampling rate
+        target_t = np.arange(ONSET_T,OFFSET_T,1/SR)
+        f = interp1d(timestamps, rawdata, kind='nearest', bounds_error = False, fill_value=np.nan)
+        signal_interp = f(target_t)
+        
+        #SR = float(info["nominal_srate"][0])  # info['effective_srate']
+        units = 'unknown'
+        desc = info.get('desc',[])
+        if desc and desc[0]:
+            units = desc[0]["channels"][0]["channel"][0]["unit"][0]
 
         hdr = {
-            "id": p,
-            "participant": p,
+            "id": stream_name,
+            "participant": stream_name,
             "sampling_frequency": SR,
             "modality": modality,
             "units": units,
         }
-        dat = rawdata
+        dat = signal_interp
         bio.add_channel((hdr, np.array(dat)))
 
-    bio.meta["date"] = header["info"]["datetime"][0]
+    if 'datetime' in header['info']:
+        bio.meta["date"] = stringify(header["info"]["datetime"])
 
     return bio
 
 
-def get_onset(streams):
+
+
+
+
+
+def get_onset_offset(streams):
     """
     One issue is that in XDF, streams can start at different moments in time.
     Here we need to align them all to a common offset.
-    The idea is to find the earliest moment in time where all streams have started,
-    and then cut off anything in any stream before that.
-    It throws away some data (downside) but allows us to have everything
-    aligned in time (upside).
+    The idea is to find the earliest moment in time any one stream starts.
+    Later, when we resample the data, we can simply set the streams to NA
+    before that. 
     """
 
     start_ts = [min(s["time_stamps"]) for s in streams]
-    ONSET_T = max(start_ts)
+    ONSET_T = min(start_ts)
 
-    n_samp = [sum(s["time_stamps"] >= ONSET_T) for s in streams]
-    print(n_samp)
-    N_SAMP = min(n_samp)  # take the smallest common time portion
+    end_ts = [max(s["time_stamps"]) for s in streams]
+    OFFSET_T = max(end_ts)
+    
+    #n_samp = [sum(s["time_stamps"] >= ONSET_T) for s in streams]
+    #print(n_samp)
+    #N_SAMP = min(n_samp)  # take the smallest common time portion
 
-    print("---------- TIMING ----------")
-    print("Joint start t={}".format(ONSET_T))
-    print("Stream onset deltas (should not be too great)")
-    print([t - ONSET_T for t in start_ts])
-    print("Largest common duration: {} samples".format(N_SAMP))
-    print("Duration mismatches (proportion of common duration)")
-    print(" ".join(["{:.4f}".format(n / N_SAMP) for n in n_samp]))
-    print()
+    return ONSET_T, OFFSET_T
 
-    print("---------- SAMPLING RATES ----------")
-    nominal_sr = [float(s["info"]["nominal_srate"][0]) for s in streams]
-    eff_sr = [float(s["info"]["effective_srate"]) for s in streams]
-    print("Nominal sampling rates:")
-    print(" ".join(["{:.2f}".format(n) for n in nominal_sr]))
-    print("Effective sampling rates:")
-    print(" ".join(["{:.2f}".format(n) for n in eff_sr]))
-    print()
 
-    return ONSET_T, N_SAMP
+
+
+
+
+
+def stringify(vals):
+    """
+    pyxdf tends to supply parameter values as 1-item arrays for some reason.
+    e.g.
+    'name': ['SendDataC'],
+    'type': ['EEG']
+    
+    Biobabel doesn't use that system, so here, to avoid throwing away data,
+    let's concatenate the strings into a single value.
+    """
+    return " ".join([ str(s) for s in vals])
+
+
